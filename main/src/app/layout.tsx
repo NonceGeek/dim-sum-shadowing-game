1d91055f3ba373e17ac9e82f20d55051594aade1
leeduckgo <leeduckgo@gmail.com>
2026-06-11 20:37:22 +0800

    update with app name

--- main/src/app/layout.tsx
+++ main/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Dimsum Shadowing Game",
-  description: "Dimsum Shadowing Game",
+  title: "跟住读",
+  description: "听一句，学一句，超有效！",
 };
 
 export default function RootLayout({
