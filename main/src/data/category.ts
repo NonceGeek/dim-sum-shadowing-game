0f6233147f6ebd7a5baad385bdb44609cb0de60b
leeduckgo <leeduckgo@gmail.com>
2026-07-03 18:12:05 +0800

    update with main/src/data/category.ts

--- main/src/data/category.ts
+++ main/src/data/category.ts
@@ -4,7 +4,7 @@
     name: "饮食",
     questions: [
       {
-        content: "我(ngo5)唔(m4)食(sik6)辣(laat6)嘢(ye5)",
+        content: "我(ngo5)唔(m4)食(sik6)辣(laat6)嘢(je5)",
         originalText: "我不吃辣的东西",
         yueText: "我唔食辣嘢",
         audioUrl: "/audio/yue1.m4a",
@@ -22,7 +22,7 @@
     name: "问路",
     questions: [
       {
-        content: "依(ji1)个(go3)地(dei6)址(zi2)点(dim2)行(hang4)",
+        content: "依(ji1)个(go3)地(dei6)址(zi2)点(dim2)行(haang4)",
         originalText: "这个地址怎么走？",
         yueText: "依个地址点行",
         audioUrl: "/audio/yue15.m4a",
@@ -35,7 +35,7 @@
     questions: [
       {
         content:
-          "广(gwon2)州(zau1)灯(dang1)光(gwong1)秀(sau3)型(jing4)到(dou3)爆(baau3)",
+          "广(gwong2)州(zau1)灯(dang1)光(gwong1)秀(sau3)型(jing4)到(dou3)爆(baau3)",
         originalText: "广州灯光秀很酷炫",
         yueText: "广州灯光秀型到爆",
         audioUrl: "/audio/yue21.m4a",
@@ -60,9 +60,9 @@
     questions: [
       {
         content:
-          "去(heoi3)广(gwong2)州(zau1)塔(taap3)大(daai6)概(koi3)要(jiu3)几(gei2)耐(noi6)啊(aa)",
-        originalText: "去广州塔大概要多久啊？",
-        yueText: "去广州塔大概要几耐啊",
+          "去(heoi3)广(gwong2)州(zau1)塔(taap3)大(daai6)概(koi3)要(jiu3)几(gei1)耐(noi6)",
+        originalText: "去广州塔大概要多久？",
+        yueText: "去广州塔大概要几耐",
         audioUrl: "/audio/yue30.m4a",
       },
     ],
@@ -72,7 +72,7 @@
     name: "询问",
     questions: [
       {
-        content: "点(im2)解(gaai2)嘅(ge2)",
+        content: "点(dim2)解(gaai2)嘅(ge3)",
         originalText: "为什么呢？",
         yueText: "点解嘅",
         audioUrl: "/audio/yue35.m4a",
@@ -84,7 +84,7 @@
     name: "回答",
     questions: [
       {
-        content: "冇(u5)问(man6)题(tai4)",
+        content: "冇(mou5)问(man6)题(tai4)",
         originalText: "没问题",
         yueText: "冇问题",
         audioUrl: "/audio/yue42.m4a",
